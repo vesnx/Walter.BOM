0f2031ab794ea252dc6f6826712ffe30c313f821
VESNX SA <130899007+vesnx@users.noreply.github.com>
2024-03-03 14:28:31 +0100

    Checkin sample code

---
+++ samples/ExceptionConsoleSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+namespace ExceptionConsoleSample
+{
+    internal class Program
+    {
+        static IServiceProvider _serviceProvider;
+        static Program()
+        {
+            _serviceProvider = new ServiceCollection()
+                                .AddLogging(setup => {
+                                    setup.AddConsole();
+                                    setup.SetMinimumLevel(LogLevel.Information);
+                                })
+                                .LogNotImplementedByDesignException(api:"https://localhost:5001")
+                               .BuildServiceProvider();
+            //add logging to the walter framework
+            _serviceProvider.AddLoggingForWalter();
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ samples/NotImplementedByDesign/Program.cs
@@ -0,0 +1,74 @@
+﻿// ***********************************************************************
+// Assembly         : 
+// Author           : Walter Verhoeven
+// Created          : Sun 03-Mar-2024
+//
+// Last Modified By : Walter Verhoeven
+// Last Modified On : Sun 03-Mar-2024
+// ***********************************************************************
+// <copyright file="Program.cs" company="VESNX SA">
+//     Copyright (c) VESNX SA. All rights reserved.
+// </copyright>
+// <summary>
+// shows the use of NotImplementedByDesignException in AoT compatible mode
+// with the library being trimmed and published using AoT 
+// </summary>
+// ***********************************************************************
+using System.Net;
+
+using Walter.BOM;
+public class NetworkService
+{
+    public void CheckIPAddressSupport(IPAddress ipAddress)
+    {
+        if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+        {
+            // IPv6 is found to be out of scope for the current release
+            throw new NotImplementedByDesignException("IPv6 support is out of scope for the current release.");
+        }
+        else
+        {
+            Console.WriteLine("IPv4 address is supported.");
+        }
+    }
+}
+
+// Usage example
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        using var serice= 
+
+        var color= Console.ForegroundColor;
+        var networkService = new NetworkService();
+        try
+        {
+            // Assuming an IPv6 address for demonstration
+            IPAddress ipv6Address = IPAddress.Parse("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+            networkService.CheckIPAddressSupport(ipv6Address);
+        }
+        catch (NotImplementedByDesignException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Please contact support with the following details:");
+            Console.WriteLine($"Member Name: {ex.CallerMemberName}");
+            Console.WriteLine($"File Path: {ex.FilePath}");
+            Console.WriteLine($"Line: {ex.Line}");
+        }
+        finally 
+        {
+#if !DEBUG
+            Console.ForegroundColor = color;
+            Console.WriteLine();
+
+Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
